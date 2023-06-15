711770b6dd99d65f7b63cae5bb00c62a31e83008
gopinadhkonduri <kgopinadh018@gmail.com>
2023-06-15 19:23:55 +0530

    added/modified/deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,12 @@
 package pages;
 
 public class LoginPage {
+	public static void main(String[] args) {
+		System.out.println("hello login page");
+		String un="admin";
+		String pwd="admin";
+		System.out.println("logged in");
+		
+	}
 
 }
